db54c03bd41d19bd16db82e4efe3e2a4de801d4f
gayatrizankar <gayatrizankar30@gmail.com>
2025-04-11 08:54:10 +0530

    fix: use process.env.NODE_ENV for BASE_URL in CRA

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,4 +1,9 @@
+import axios from "axios"; // ✅ Don't forget this import
+
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:5000/api' : "/api",
-    withCredentials: true,
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5000/api"
+      : "/api",
+  withCredentials: true,
 });
